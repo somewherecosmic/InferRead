56efda53c43cecaad046f64ae168ed294c1cc4da
mcquilr4 <ryan.mcquillan4@mail.dcu.ie>
2022-11-10 17:45:37 +0000

    nav-bar generated and mostly styled

--- src/infer-read-frontend/src/app/app-routing.module.ts
+++ src/infer-read-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavBarGeneralComponent } from './components/nav-bar-general/nav-bar-general.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'nav', component: NavBarGeneralComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/infer-read-frontend/src/app/app.module.ts
+++ src/infer-read-frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { NavBarGeneralComponent } from './components/nav-bar-general/nav-bar-general.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    NavBarGeneralComponent
   ],
   imports: [
     BrowserModule,
