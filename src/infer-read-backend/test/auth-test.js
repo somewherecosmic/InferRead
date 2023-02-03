7a5dcd6c6d0a1d11a3b0d2717904e423a72c70c0
Ethan Hall <ethan.hall23@mail.dcu.ie>
2023-02-03 19:16:29 +0000

    IR_52 Validation

---
+++ src/infer-read-backend/test/auth-test.js
@@ -0,0 +1,63 @@
+import mongoose from 'mongoose';
+import { signup, logIn } from '../controllers/auth.js';
+import User from '../models/User.js';
+import { expect, should } from 'chai'; 
+
+
+// Create new User in before block for login validation
+// Have signup validation handle the creation itself through req, then deleteMany at end
+
+describe('Auth Controller Validation - Login', () => {
+
+    // Need to export this logic to a helper for the sake of DRY
+    before((done) => {
+        mongoose.set('strictQuery', false);
+        mongoose.connect(
+        'mongodb+srv://Ethan:2mQWDO8L7jTetddl@cluster0.kbsgj.mongodb.net/validation?retryWrites=true&w=majority'
+        ).then(() => {
+            const user = new User({
+                "email": 'test@test.com',
+                "password": 'test'
+            });
+            return user.save(done());
+        })
+    });
+
+    it("Should return a jwt token with status 200", async () => {
+        const req = { body: {
+            "email": "test@test.com",
+            "password": 'test'
+        }
+        };
+        const res = {
+            email: null,
+            token: null,
+            id: null,
+            expiresIn: null,
+            statusCode: null,
+            status: function(code) {
+                this.statusCode = code;
+            },
+            json: function(data) {
+                this.email = data.email;
+                this.token = data.token;
+                this.id = data.id;
+                this.expiresIn = data.expiresIn;
+                return this;
+            }
+        };
+        const result = await logIn(req, res, () => {})
+
+        if (result) {
+            expect(status).to.be.equal(200);
+            expect(res.email).to.be.equal(req.email);
+            should.exist(res.token);
+        }
+    });
+
+    after((done) => {
+        User.deleteMany();
+        mongoose.disconnect(done());
+    })
+
+})
