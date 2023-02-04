9a055c205e9e0df5b77a4c45e314fb0742eb8c6d
Ryan Mc Quillan <ryan.mcquillan4@mail.dcu.ie>
2023-02-04 12:59:00 +0000

    2_IR_44: Redo Routing

--- src/infer-read-frontend/src/app/app-routing.module.ts
+++ src/infer-read-frontend/src/app/app-routing.module.ts
@@ -8,19 +8,26 @@ import { LoginAuthorizationComponent } from './components/login-auth/login-auth.
 import { HomeComponent } from './components/home/home.component';
 import { AuthGuard } from './services/auth-guard/auth.guard';
 
-
 @NgModule({
-  imports: [RouterModule.forRoot(
-    [
-      { path: 'overview', component: OverviewComponent, canActivate: [AuthGuard] },
-      { path: 'read', component: ReadingViewComponent },
-      { path: 'upload', component: UploadPageComponent },
-      { path: 'review', component: ReviewPageComponent },
-      {path: 'auth', component: LoginAuthorizationComponent },
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-    ],
-    { initialNavigation: 'enabledBlocking'}
-  )],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(
+      [
+        { path: '', component: LoginAuthorizationComponent },
+        {
+          path: '',
+          canActivate: [AuthGuard],
+          children: [
+            { path: 'overview', component: OverviewComponent },
+            { path: 'read', component: ReadingViewComponent },
+            { path: 'upload', component: UploadPageComponent },
+            { path: 'review', component: ReviewPageComponent },
+          ],
+        },
+        { path: '**', redirectTo: '' },
+      ]
+      //{ initialNavigation: 'enabledBlocking'}
+    ),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
