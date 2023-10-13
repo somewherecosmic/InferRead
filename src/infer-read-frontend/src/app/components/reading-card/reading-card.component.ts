5eb9dbc68dc5bc1fe9089dc0c94a46861f2a8620
Ethan Hall <ethanhall7742@gmail.com>
2023-10-13 18:11:02 +0100

    Added outline for changing document title on the overview screen

--- src/infer-read-frontend/src/app/components/reading-card/reading-card.component.ts
+++ src/infer-read-frontend/src/app/components/reading-card/reading-card.component.ts
@@ -1,13 +1,30 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-reading-card',
   templateUrl: './reading-card.component.html',
   styleUrls: ['./reading-card.component.scss']
 })
-export class ReadingCardComponent {
+export class ReadingCardComponent implements OnInit {
   @Input() id: any;
   @Input() title: string;
   @Input() language: string;
   @Input() length: number;
+
+  editing = false;
+  
+  @ViewChild('titleInput') titleInput: ElementRef;
+
+
+  ngOnInit() {
+  }
+
+  editTitle() {
+    this.editing = true;
+  }
+
+  saveChanges(event) {
+    console.log(event.target.value);
+    this.editing = false;
+  }
 }
